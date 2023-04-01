9d08828b68b6f8e29b29ba438ddb17715b8e5819
Tutorials With Gary <lukeberry9919@gmail.com>
2023-04-01 10:39:48 +1030

    Moved the resources into the correct location & got the first image rendering.

--- baba_main/src/main.rs
+++ baba_main/src/main.rs
@@ -1,31 +1,53 @@
 mod ecs;
 mod data_structures;
 use bevy::prelude::*;
+use bevy::window::PrimaryWindow;
 use ecs::systems::index_debugger;
 use ecs::systems::index_position_updater;
-use data_structures::enums::nodes::noun::*;
-use  data_structures::enums::node::Node;
 
 fn main() {
-    let test_node: Node = Node::Noun(Noun::Baba);
-
-    match test_node {
-        Node::Noun(noun) => {},
-        Node::Operator(operator) => {},
-        Node::Property(property) => {},
-    };
-
     App::new()
     .add_plugins(DefaultPlugins)
     .add_startup_system(setup)
+    .add_startup_system(setup_camera)
     .add_system(index_position_updater::update)
     .add_system(index_debugger::index_debugger_print)
     .run();
 }
 
-pub fn setup(mut commands : Commands){
+pub fn setup(
+    mut commands : Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>){
+
+    let window = window_query.get_single().unwrap();
+    let x = window.width() / 2.0;
+    let y = window.height() / 2.0;
+
     commands.spawn((
-        ecs::components::position_index::PositionIndex{x: 2, y: 2 },
-        ecs::components::position::Position{x: 2.0, y: 2.0 }
+        ecs::components::position_index::PositionIndex{ x: 2, y: 2 },
+        ecs::components::position::Position{ x: 2.0, y: 2.0 }
     ));
+
+    let asset = asset_server.load("BabaIsYouIcon.png");
+
+    commands.spawn(SpriteBundle{
+        transform: Transform::from_xyz(x, y, 0.0),
+        texture: asset,
+        ..Default::default()
+    });
+}
+
+pub fn setup_camera(
+    mut commands : Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>){
+
+    let window = window_query.get_single().unwrap();
+    let x = window.width() / 2.0;
+    let y = window.height() / 2.0;
+
+    commands.spawn(Camera2dBundle{
+        transform: Transform::from_xyz(x, y, 0.0),
+        ..Default::default()
+    });
 }
